723169c5efcf9d21df5dc9302d2cf6336d96dd5b
David <davavila@cisco.com>
2023-11-17 13:21:36 -0700

    update to add deleteAlbum function

--- main.go
+++ main.go
@@ -13,20 +13,21 @@ type album struct {
     Price  float64 `json:"price"`
 }
 
+func main() {
+    router := gin.Default()
+    router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumByID)
+	router.DELETE("/albums/:id", deleteAlbums)
+    router.POST("/albums", postAlbums)
+
+    router.Run("localhost:8080")
+}
+
 // albums slice to seed record album data.
 var albums = []album{
     {ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
     {ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
     {ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-}
-
-func main() {
-    router := gin.Default()
-    router.GET("/albums", getAlbums)
-	router.GET("/albums/:id", getAlbumByID)
-    router.POST("/albums", postAlbums)
-
-    router.Run("localhost:8080")
 }
 
 // getAlbums responds with the list of all albums as JSON.
@@ -63,4 +64,20 @@ func getAlbumByID(c *gin.Context) {
         }
     }
     c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+}
+
+func deleteAlbums(c *gin.Context) {
+    id := c.Param("id")
+
+    // Loop over the list of albums, looking for
+	// an album whose ID value matches the parameter.
+    
+	for i, a := range albums {
+		if a.ID == id {
+			albums = append(albums[:i], albums[i+1:]...)
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}	
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
 }
